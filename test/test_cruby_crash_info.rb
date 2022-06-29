488886fc00edfc2afe0c35694c823447234c266e
John Hawthorn <john@hawthorn.email>
2022-06-29 15:59:55 -0700

    Add actual tests

--- test/test_cruby_crash_info.rb
+++ test/test_cruby_crash_info.rb
@@ -3,11 +3,30 @@
 require "test_helper"
 
 class TestCrubyCrashInfo < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::CrubyCrashInfo::VERSION
+  def test_info_can_be_set_and_retrieved
+    CRubyCrashInfo.info = "extra info"
+    assert_equal "extra info", CRubyCrashInfo.info
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_info_is_reported_on_crash
+    p, c = IO.pipe
+    opts = {
+      err: c
+    }
+    pid = Process.spawn(RbConfig.ruby, "-I", "#{__dir__}/../lib", "-e", <<~RUBY, opts)
+      require "cruby_crash_info"
+      require "fiddle"
+
+      CRubyCrashInfo.info = "Important extra info"
+
+      # Invalid pointer deference! Should SEGV
+      Fiddle::Pointer[0x1000][0]
+    RUBY
+    c.close
+
+    Process.waitpid(pid)
+
+    err = p.read
+    assert_includes err, "Important extra info"
   end
 end
